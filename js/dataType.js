a1c1e256fcdd7c96f198a94df02dfbbdab22e8b4
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-11-01 17:10:54 +0600

    data type in js

---
+++ js/dataType.js
@@ -0,0 +1,14 @@
+// data type in js 
+// primitive data
+// 1. string
+console.log('my name is mehedi')
+// 2. number
+console.log(23)
+// 3. boolean
+console.log(true)
+// 4. null
+console.log(null)
+// 5. undefined
+console.log(undefined)
+console.log("")
+
