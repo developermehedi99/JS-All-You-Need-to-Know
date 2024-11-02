ce280dff880e7e37adaa1a5e90be2468014e13a2
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-11-02 15:19:01 +0600

    date detaill

--- js/date.js
+++ js/date.js
@@ -1,1 +1,14 @@
-console.log('today is frinday')
+
+var date = new Date();
+console.log(date.toDateString());
+console.log(date.toTimeString());
+console.log(date.toLocaleString())
+
+console.log(date.getFullYear())
+console.log(date.getMonth())
+console.log(date.getDate())
+
+// const date = new Date();
+console.log(date.getHours())
+console.log(date.getMinutes())
+console.log(date.getSeconds())
