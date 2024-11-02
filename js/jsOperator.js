3d89b82cc9676db533f65f8d573e51abe09edef2
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-11-02 14:50:06 +0600

    js operator

---
+++ js/jsOperator.js
@@ -0,0 +1,20 @@
+// js operator
+// Operator------Description
+// +	Addition
+// -	Subtraction
+// *	Multiplication
+// **	Exponentiation (ES2016)
+// /	Division
+// %	Modulus (Division Remainder)
+// ++	Increment
+// --	Decrement
+
+// ==	equal to
+// ===	equal value and equal type
+// !=	not equal
+// !==	not equal value or not equal type
+// >	greater than
+// <	less than
+// >=	greater than or equal to
+// <=	less than or equal to
+// ?	ternary operator
