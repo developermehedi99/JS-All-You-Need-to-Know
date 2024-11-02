045ecf4ea32bb8d0a7f5262fd205cc9683bfba13
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-11-02 15:22:19 +0600

    js comments

---
+++ js/comments.js
@@ -0,0 +1,13 @@
+
+// console.log(date.getHours())
+
+// console.log(date.getMinutes())
+// console.log(date.getSeconds())
+
+/*
+console.log(date.getHours())
+console.log(date.getMinutes())
+console.log(date.getSeconds())console.log(date.getHours())
+console.log(date.getMinutes())
+console.log(date.getSeconds())
+*/
