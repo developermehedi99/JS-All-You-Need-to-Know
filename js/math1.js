23bbfe410c1ae4598a24772f512f264efd18749a
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-11-02 15:10:01 +0600

    math js

---
+++ js/math1.js
@@ -0,0 +1,10 @@
+
+var n = 4.623;
+console.log(Math.abs(n));     // Absolute value
+console.log(Math.ceil(n));    // Rounds up to the next whole number
+console.log(Math.floor(n));   // Rounds down to the previous whole number
+console.log(Math.round(n));   // Rounds to the nearest whole number
+
+console.log(Math.max(23, 45, 87, 43, 93, 63));  // Finds the maximum value
+console.log(Math.min(23, 45, 34, 13, 49, 47, 234));  // Finds the minimum value
+
