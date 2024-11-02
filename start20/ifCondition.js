70f29dcd3271392f5eab0276db54479d8d187cce
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-11-02 17:16:16 +0600

    if else condition

---
+++ start20/ifCondition.js
@@ -0,0 +1,18 @@
+var a =20;
+var b=20;
+if(a > b){
+    console.log('a is getter than b' )
+}else if(a< b){
+    console.log('b is getter than a')
+} else{
+    console.log('both are same')
+}
+
+let n = 9;
+if(n % 2 === 0){
+    console.log('n is even number')
+}else if(n % 2 === 1){
+    console.log('n is odd number')
+} else{
+    console.log('n  is zero')
+}
